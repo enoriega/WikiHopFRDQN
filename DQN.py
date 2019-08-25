883ddbcd794526d54df7c23a7713b6ac15d2d6cc
Enrique Noriega <enrique.noriega@gmail.com>
2019-08-24 17:54:06 -0700

    Removed some files from the version and added the python gitignore file

---
+++ DQN.py
@@ -0,0 +1,10 @@
+import torch
+import bcolz
+import pickle
+import numpy as np
+
+glove_path = "/Users/enrique/github/WikiHopFR/glove"
+
+if __name__ == "__main__":
+    # preprocess_embeddings()
+    pass
