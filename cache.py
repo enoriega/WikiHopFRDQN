e6ea574c8cfab4bf2e93402a4248ee808d89cb54
Enrique Noriega <enrique.noriega@gmail.com>
2020-01-06 23:14:31 -0700

    Nerfed the cache to save memory

--- cache.py
+++ cache.py
@@ -7,21 +7,23 @@ class Cache:
         self.queue = list()
 
     def __contains__(self, identifier):
-        return identifier in self.backend
+        # return identifier in self.backend
+        return False
 
     def __setitem__(self, key, value):
-        queue = self.queue
-
-        # Forget some previously cached elements to avoid memory leaks
-        if len(queue) >= self._size:
-            del_size = self._size // 10
-            to_delete = queue[:del_size]
-            self.queue = queue[del_size:]
-            for k in to_delete:
-                del self.backend[k]
-        self.queue.append(key)
-
-        self.backend[key] = value
+        # queue = self.queue
+        #
+        # # Forget some previously cached elements to avoid memory leaks
+        # if len(queue) >= self._size:
+        #     del_size = self._size // 10
+        #     to_delete = queue[:del_size]
+        #     self.queue = queue[del_size:]
+        #     for k in to_delete:
+        #         del self.backend[k]
+        # self.queue.append(key)
+        #
+        # self.backend[key] = value
+        pass
 
     def __getitem__(self, item):
         return self.backend[item]
